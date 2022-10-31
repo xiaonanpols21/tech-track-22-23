0c61f497fe2d2337f2062a673e7dec372b6a8d59
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-10-31 16:08:12 +0100

    Codepen code

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,42 @@ import '../styles/style.css'
 import * as d3 from 'd3';
 
 console.log('Hello, world!');
+
+const seconds = 10;
+
+function getData() {
+	console.log('Grabbing new userdata...');
+	
+	fetch('https://opensheet.elk.sh/1tO9SS1f9RQZ0Ay0BmkczJVkCxWcAhzGtcSDpzheKTZI/responses')
+		.then(res => res.json())
+		.then(data => {
+					
+			const newArray = [];
+			
+			data.forEach(item => {
+				newArray.push(item["Welke kleur kledingstukken heb je aan vandaag? (Meerdere antwoorden mogelijk natuurlijk...)"])
+			})
+					
+			let editedArray = newArray.map(item => item.split(','));
+			console.log(editedArray.flat())
+		
+			let clothes = {};
+		
+			editedArray.flat().forEach(color => {
+				color = color.trim();
+				
+				if(!clothes[color]) {
+					clothes[color] = 0;
+				}
+				
+				clothes[color] = clothes[color] + 1;
+			})
+		
+			console.log(clothes);
+		
+		});
+	}
+
+// setInterval(getData, seconds * 1000)
+
+getData();
