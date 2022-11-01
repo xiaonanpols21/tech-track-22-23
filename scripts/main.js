f547e97aba20576cb277780b5aad0d2930a0182d
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-11-01 21:23:36 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 // Our bundler automatically creates styling when imported in the main JS file!
 import '../styles/style.css'
 
-
-
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
 
