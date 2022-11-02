a5deac1fa3af2f3ecdbd0f3f2d2d5490c2c2e003
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-11-02 15:06:08 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@ import * as d3 from 'd3';
 
 console.log('Hello, world!');
 
-// Album
 const options = {
 	method: 'GET',
 	headers: {
@@ -15,20 +14,13 @@ const options = {
 	}
 };
 
+// Album
 fetch('https://spotify23.p.rapidapi.com/artist_albums/?id=3Nrfpe0tUJi4K4DXYWgMUX&offset=0&limit=100', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
 // Overview
-const options2 = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c20e05d39emsh51bf7509082730ep146d0cjsn622276aaec1a',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-	}
-};
-
 fetch('https://spotify23.p.rapidapi.com/artist_overview/?id=3Nrfpe0tUJi4K4DXYWgMUX', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
