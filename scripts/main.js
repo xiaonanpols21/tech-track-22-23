08bd8b5ed9b955ffcc7398a1aa299f1642cabb52
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-11-02 13:21:24 +0100

    Spotify API Rapid Api

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ import '../styles/style.css'
 import * as d3 from 'd3';
 
 console.log('Hello, world!');
-
+/*
 const options = {
 	method: 'GET',
 	headers: {
@@ -15,6 +15,33 @@ const options = {
 };
 
 fetch('https://k-pop.p.rapidapi.com/boy-groups?q=BTS&by=Group%20Name', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+*/
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c20e05d39emsh51bf7509082730ep146d0cjsn622276aaec1a',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+fetch('https://spotify23.p.rapidapi.com/artist_albums/?id=3Nrfpe0tUJi4K4DXYWgMUX&offset=0&limit=100', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+const options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c20e05d39emsh51bf7509082730ep146d0cjsn622276aaec1a',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+fetch('https://spotify23.p.rapidapi.com/artist_overview/?id=3Nrfpe0tUJi4K4DXYWgMUX', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
