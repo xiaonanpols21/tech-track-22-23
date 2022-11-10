3ebf95ac91abaceea3b0e31f07dff893b04988c1
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-11-10 09:05:53 +0100

    intersection observer werkt nog nii

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ const main = document.querySelector("main");
 // Dark Mode
 function darkMode() {
 	body.classList.toggle("dark-mode");
-}
+};
 darkBtn.addEventListener("click", darkMode);
 
 // Fetch data, Stackoverflow https://stackoverflow.com/questions/31710768/how-can-i-fetch-an-array-of-urls-with-promise-all
@@ -44,35 +44,94 @@ Promise.all(urls.map(u=>fetch(u))).then(responses =>
 
 	console.log(data);
 	changeData(data, albums);
-})
+});
 
 // Show to the HTML
 function changeData(data, albums) {
+	// Header
 	bandName.textContent = data[1].data.artist.profile.name;
 	followers.textContent = data[1].data.artist.stats.followers;
 	listeners.textContent = data[1].data.artist.stats.monthlyListeners;
 	profilePic.src = data[1].data.artist.visuals.gallery.items[0].sources[0].url;
 
-	// Poging 3
-	albums.forEach((element) => {
-		const albumImg = element.coverArt.sources[2].url;
-		const year = element.date.year;
-		const name = element.name;
-		// TODO: Play doet nog nii
-		const play = element.sharingInfo.shareUrl;
-		console.log(play);
+	// ForEach Albums
+	albums.forEach((item) => {
+		const albumImg = item.coverArt.sources[2].url;
+		const year = item.date.year;
+		const name = item.name;
+		const play = item.sharingInfo.shareUrl;
 
-		const html = `
-		<article>
+		const html = 
+		`<article class="${year} card">
 			<h2>${year}</h2>
 			<h3>${name}</h3>
-			<img src="${albumImg}" alt="BTS Album">
+			<img src="${albumImg}" alt="${name}">
 			<a class="play" href="${play}"><i class="fa-solid fa-play"></i></a>
-		</article>
-		`
+		</article>`
 		main.insertAdjacentHTML("beforeend", html );
+		// Web API, insertAdjacentHTML is om het te tonen in de main. Beforeend betekend: Before the end of the element (last child), W3Schools https://www.w3schools.com/jsref/met_node_insertadjacenthtml.asp
 	});
+};
 
-} 
+/* W3Schools filter
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.querySelector("article");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
 
-// TODO: foreach maken, play btn, filter year slider
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.querySelector("nav");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+*/
+
+
+const cards = document.querySelectorAll(".card")
+console.log(cards)
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		entry.target.classList.toggle("show", entry.isIntersecting)
+	})
+})
+
+cards.forEach((card) => {
+	observer.observe(card)
+})
+
+
+
+// TODO: , filter year slider, show cards api see project tech
