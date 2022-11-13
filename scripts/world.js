204c8d0421fdd0f23c1ef4ac1c4ddd8a94b673be
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-11-13 17:10:59 +0100

    begin d3 map

---
+++ scripts/world.js
@@ -0,0 +1,41 @@
+import '../styles/style.scss'
+import * as d3 from 'd3';
+import { html, thresholdScott } from 'd3';
+
+// Data
+const urls = ['./data/bts-albums.json', './data/bts-overview.json'];
+
+// Dark Mode
+const darkBtn = document.querySelector("header button");
+const body = document.querySelector("body");
+
+// Dark Mode
+function darkMode() {
+	body.classList.toggle("dark-mode");
+};
+darkBtn.addEventListener("click", darkMode);
+
+// Fetch data, Stackoverflow https://stackoverflow.com/questions/31710768/how-can-i-fetch-an-array-of-urls-with-promise-all
+Promise.all(urls.map(u=>fetch(u))).then(responses =>
+    Promise.all(responses.map(res => res.json()))
+).then(data => {
+
+	// Make new array from only the Albums
+	const albums = [];
+	data[0].data.artist.discography.albums.items.forEach(item => {
+		albums.push(item.releases.items[0]);
+	});
+
+	// Sort from debut to present, Freecodecamp
+	function order(albums) {
+		return albums.sort(function(a, b) {
+			return a === b ? 0 : a < b ? 1 : -1;
+		})
+	}
+	console.log(order(albums));
+
+	console.log(data);
+});
+
+
+
